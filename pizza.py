59ae13de1c194f5b4df12671b710fec8ead4e4c6
dragbone <ddragbone@gmail.com>
2016-08-22 20:45:17 +0200

    Added memory to bot

--- pizza.py
+++ pizza.py
@@ -1,5 +1,6 @@
 import plugins
 import random
+from operator import add
 
 def _initialise(bot):
     plugins.register_user_command(["vote","pizza"])
@@ -20,27 +21,38 @@ dayToIndex = {"mo":0,
               }
 
 def pizza(bot, event, *args):
-    indices = [i for i, x in enumerate(bot.votes) if x == max(bot.votes)]
+    votes = [0] * 5
+    if bot.memory.exists(["pizzavotes"]):  # verify key exists
+        votes = bot.memory.get_by_path(["pizzavotes"])  # grab all votes
+        for user_id in votes:
+            vote = votes[user_id]
+            votes = map(add, votes, vote)
+
+    indices = [i for i, x in enumerate(votes) if x == max(votes)]
     days = map(lambda x:indexToDay[x], indices)
     yield from bot.coro_send_message(event.conv, _("Pizzaday could be on {}").format(days))
     yield from bot.coro_send_message(event.conv, _("I recommend to eat pizza on {}").format(random.choice(days)))
 
 def resetvote(bot, event, *args):
-    bot.votes = [0]*5
+    counter = 0
+    if bot.memory.exists(["pizzavotes"]):  # verify key exists
+        votes = bot.memory.get_by_path(["pizzavotes"])  # grab all votes
+        for user_id in votes:
+            bot.memory.pop_by_path(["pizzavotes", user_id])
+            counter += 1
+    bot.memory.save()
+    yield from bot.coro_send_message(event.conv, _("Deleted {} vote(s)").format(counter))
 
 def vote(bot, event, *args):
-    """
-    Vote for the next pizza day
-
-    /bot vote <i>Mon-Wed,Fri</i>
-    """
-
     if not args:
         yield from bot.coro_send_message(event.conv, _("Usage: /bot vote <i>Mon-Wed,Fri</i>"))
         return
 
     try:
-        voteForPizza(bot, ",".join(args))
+        vote = voteForPizza(bot, ",".join(args))
+        bot.memory.set_by_path(["pizzavotes", event.user.id_.chat_id], vote)
+        bot.memory.save()
+        """bot.user_memory_set(event.user.id_.chat_id, 'pizzavote', vote)"""
         yield from bot.coro_send_message(event.conv, _("Thank you for voting."))
     except ValueError:
         yield from bot.coro_send_message(event.conv, _("Error: Do you even syntax."))
@@ -50,13 +62,16 @@ def voteForPizza(bot, inp):
     groups = inp.lower().split(",")
     days = map(lambda x:x.split("-"), groups)
     ranges = map(lambda x:map(lambda y:dayToIndex[y[:2]], x), days)
-
+    vote = [0]*5
     for ri in ranges:
         r = list(ri)
         if len(r) == 1:
-            bot.votes[r[0]] += 1
+            vote[r[0]] += 1
         elif len(r) == 2:
             for d in range(r[0], r[1] + 1):
-                bot.votes[d] += 1
+                vote[d] += 1
         else:
             raise ValueError("urgh")
+    #Clean
+    vote = map(lambda x:min(x, 1), vote)
+    return vote
