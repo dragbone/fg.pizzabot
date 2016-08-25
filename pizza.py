c7da0e61ede1e7c66e1fe8dfbe47ee9bdf1ab0fd
dragbone <ddragbone@gmail.com>
2016-08-25 20:47:24 +0200

    Moved all code to single file because that's how hangoutsbot wants it :(

--- pizza.py
+++ pizza.py
@@ -1,14 +1,14 @@
 import plugins
-import pizzaparser
+import re
+import random
+import datetime
 from operator import add
 
 MEM_NAME = "pizzavotes"
 
-
 def _initialise(bot):
     plugins.register_user_command(["vote", "pizza"])
     plugins.register_admin_command(["resetvotes", "dumpvotes"])
-
 
 indexToDay = {0:"Monday",
               1:"Tuesday",
@@ -29,7 +29,7 @@ def pizza(bot, event, *args):
     days = list(map(lambda x:indexToDay[x], indices))
     yield from bot.coro_send_message(event.conv, _("Pizzaday could be on {}").format(days))
 
-    recommendedDay = indexToDay[pizzaparser.choosePizzaDay(indices)]
+    recommendedDay = indexToDay[choosePizzaDay(indices)]
     yield from bot.coro_send_message(event.conv, _("I recommend to eat pizza on {}").format(recommendedDay))
 
 def dumpvotes(bot, event, *args):
@@ -61,9 +61,66 @@ def vote(bot, event, *args):
         return
 
     try:
-        vote = pizzaparser.parsePizzaVote(args)
+        vote = parsePizzaVote(args)
         bot.memory.set_by_path([MEM_NAME, event.user.id_.chat_id], vote)
         bot.memory.save()
         yield from bot.coro_send_message(event.conv, _("Thank you for voting ({})").format(vote))
     except Exception as ex:
         yield from bot.coro_send_message(event.conv, _("Error: Do you even syntax?! " + ex))
+
+# PIZZAPARSER
+# Doesn't actually parse pizzas
+
+noVote = ["none", "null", "{}", "()", "nada", "never", ":-(", ":'-("]
+
+dayToIndex = {"mo":0,
+              "di":1, "tu":1,
+              "mi":2, "we":2,
+              "do":3, "th":3,
+              "fr":4}
+
+class VoteRange:
+    def __init__(self, text):
+        match = re.match("\((.+)\)", text)
+        if match is None:
+            value = 1.0
+            inner = text
+        else:
+            value = 0.5
+            inner = match.group(1)
+
+        days = list(map(lambda x : dayToIndex[x[:2]], inner.split("-")))
+        self.vote = [0]*5
+        if len(days) == 1:
+            self.vote[days[0]] += value
+        elif len(days) == 2 and days[0] < days[1]:
+            for day in range(days[0], days[1] + 1):
+                self.vote[day] += value
+        else:
+            raise ValueError("You suck.")
+
+    def applyVote(self, votes):
+        return list(map(add, votes, self.vote))
+
+def parsePizzaVote(args):
+    vote = [0] * 5
+
+    if len(args) == 1 and args[0].lower() in noVote:
+        return vote
+
+    groups = map(lambda y: y.lower(), filter(lambda x: len(x) > 0, ",".join(args).split(",")))
+    voteRanges = map(lambda x : VoteRange(x), groups)
+    for vr in voteRanges:
+        vote = vr.applyVote(vote)
+
+    # Clean
+    vote = list(map(lambda x: min(x, 1.0), vote))
+    return vote
+
+def choosePizzaDay(days):
+    # Recommend a random day - recommended day does not change with additional votes,
+    #   as long as it is one of the most often voted ones
+    random.seed(datetime.datetime.now().isocalendar()[1])
+    dayPriorities = list(range(0, 5))
+    random.shuffle(dayPriorities)
+    return max(days, key=lambda x: dayPriorities[x])
--- pizzaparserTests.py
+++ pizzaparserTests.py
@@ -1,52 +1,52 @@
 import unittest
-import pizzaparser
+import pizza
 
 class ParserTests(unittest.TestCase):
     def test_non_vote(self):
-        self.assertEqual([0, 0, 0, 0, 0], pizzaparser.parsePizzaVote(["none"]))
+        self.assertEqual([0, 0, 0, 0, 0], pizza.parsePizzaVote(["none"]))
 
     def test_range(self):
-        self.assertEqual([1, 1, 1, 1, 1], pizzaparser.parsePizzaVote(["Mon-Fri"]))
-        self.assertEqual([1, 1, 0, 0, 0], pizzaparser.parsePizzaVote(["Mon-Tue"]))
-        self.assertEqual([0, 0, 1, 1, 1], pizzaparser.parsePizzaVote(["Wed-Fri"]))
+        self.assertEqual([1, 1, 1, 1, 1], pizza.parsePizzaVote(["Mon-Fri"]))
+        self.assertEqual([1, 1, 0, 0, 0], pizza.parsePizzaVote(["Mon-Tue"]))
+        self.assertEqual([0, 0, 1, 1, 1], pizza.parsePizzaVote(["Wed-Fri"]))
 
     def test_single(self):
-        self.assertEqual([1, 0, 0, 0, 0], pizzaparser.parsePizzaVote(["Mon"]))
-        self.assertEqual([1, 1, 0, 0, 0], pizzaparser.parsePizzaVote(["Mon","Tue"]))
-        self.assertEqual([1, 0, 0, 0, 1], pizzaparser.parsePizzaVote(["Mon","Fri"]))
+        self.assertEqual([1, 0, 0, 0, 0], pizza.parsePizzaVote(["Mon"]))
+        self.assertEqual([1, 1, 0, 0, 0], pizza.parsePizzaVote(["Mon","Tue"]))
+        self.assertEqual([1, 0, 0, 0, 1], pizza.parsePizzaVote(["Mon","Fri"]))
 
     def test_ifneeded(self):
-        self.assertEqual([0.5, 0, 0, 0, 0], pizzaparser.parsePizzaVote(["(Mon)"]))
-        self.assertEqual([0.5, 0.5, 0, 0, 0], pizzaparser.parsePizzaVote(["(Mon)","(Tue)"]))
-        self.assertEqual([0.5, 1, 0, 0, 0], pizzaparser.parsePizzaVote(["(Mon)", "Tue"]))
-        self.assertEqual([0.5, 0.5, 0.5, 0, 0], pizzaparser.parsePizzaVote(["(Mon-Wed)"]))
+        self.assertEqual([0.5, 0, 0, 0, 0], pizza.parsePizzaVote(["(Mon)"]))
+        self.assertEqual([0.5, 0.5, 0, 0, 0], pizza.parsePizzaVote(["(Mon)","(Tue)"]))
+        self.assertEqual([0.5, 1, 0, 0, 0], pizza.parsePizzaVote(["(Mon)", "Tue"]))
+        self.assertEqual([0.5, 0.5, 0.5, 0, 0], pizza.parsePizzaVote(["(Mon-Wed)"]))
 
     def test_cheater(self):
-        self.assertEqual([1, 0, 0, 0, 0], pizzaparser.parsePizzaVote(["Mon","Mon"]))
-        self.assertEqual([1, 0, 0, 0, 0], pizzaparser.parsePizzaVote(["(Mon)","(Mon)"]))
-        self.assertEqual([1, 1, 0, 0, 0], pizzaparser.parsePizzaVote(["(Mon)","Mon-Tue"]))
-        self.assertEqual([1, 1, 0, 0, 0], pizzaparser.parsePizzaVote(["Mon", "Mon-Tue"]))
-        self.assertEqual([1, 0.5, 0, 0, 0], pizzaparser.parsePizzaVote(["Mon", "(Mon-Tue)"]))
+        self.assertEqual([1, 0, 0, 0, 0], pizza.parsePizzaVote(["Mon","Mon"]))
+        self.assertEqual([1, 0, 0, 0, 0], pizza.parsePizzaVote(["(Mon)","(Mon)"]))
+        self.assertEqual([1, 1, 0, 0, 0], pizza.parsePizzaVote(["(Mon)","Mon-Tue"]))
+        self.assertEqual([1, 1, 0, 0, 0], pizza.parsePizzaVote(["Mon", "Mon-Tue"]))
+        self.assertEqual([1, 0.5, 0, 0, 0], pizza.parsePizzaVote(["Mon", "(Mon-Tue)"]))
 
     def test_parse_sep(self):
-        self.assertEqual([1, 1, 0, 0, 0], pizzaparser.parsePizzaVote(["Mon,", "Tue"]))
-        self.assertEqual([1, 1, 0, 0, 0], pizzaparser.parsePizzaVote(["Mon,Tue"]))
-        self.assertEqual([1, 1, 0, 0, 0], pizzaparser.parsePizzaVote(["Mon", "Tue"]))
+        self.assertEqual([1, 1, 0, 0, 0], pizza.parsePizzaVote(["Mon,", "Tue"]))
+        self.assertEqual([1, 1, 0, 0, 0], pizza.parsePizzaVote(["Mon,Tue"]))
+        self.assertEqual([1, 1, 0, 0, 0], pizza.parsePizzaVote(["Mon", "Tue"]))
 
     def test_parse_lan(self):
-        self.assertEqual([1, 1, 1, 1, 1], pizzaparser.parsePizzaVote(["mon", "tue", "wed", "thu", "fri"]))
-        self.assertEqual([1, 1, 1, 1, 1], pizzaparser.parsePizzaVote(["Mon", "Tue", "Wed", "Thu", "Fri"]))
-        self.assertEqual([1, 1, 1, 1, 1], pizzaparser.parsePizzaVote(["mo", "di", "mi", "do", "fr"]))
-        self.assertEqual([1, 1, 1, 1, 1], pizzaparser.parsePizzaVote(["Mo", "Di", "Mi", "Do", "Fr"]))
+        self.assertEqual([1, 1, 1, 1, 1], pizza.parsePizzaVote(["mon", "tue", "wed", "thu", "fri"]))
+        self.assertEqual([1, 1, 1, 1, 1], pizza.parsePizzaVote(["Mon", "Tue", "Wed", "Thu", "Fri"]))
+        self.assertEqual([1, 1, 1, 1, 1], pizza.parsePizzaVote(["mo", "di", "mi", "do", "fr"]))
+        self.assertEqual([1, 1, 1, 1, 1], pizza.parsePizzaVote(["Mo", "Di", "Mi", "Do", "Fr"]))
 
     def test_stable_random(self):
-        day = pizzaparser.choosePizzaDay(range(0,5))
+        day = pizza.choosePizzaDay(range(0,5))
 
         for i in range(0,5):
             # Remove day i from options, selected day must no change except if i == selected_day
             days = list(range(0,5))
             days.remove(i)
-            nDay = pizzaparser.choosePizzaDay(days)
+            nDay = pizza.choosePizzaDay(days)
             if i != day:
                 self.assertEqual(day, nDay)
             else:
@@ -54,16 +54,16 @@ class ParserTests(unittest.TestCase):
 
 class IssueTests(unittest.TestCase):
     def test_issue1_empty_vote(self):
-        self.assertEqual([0, 0, 0, 0, 0], pizzaparser.parsePizzaVote(["null"]))
-        self.assertEqual([0, 0, 0, 0, 0], pizzaparser.parsePizzaVote(["{}"]))
+        self.assertEqual([0, 0, 0, 0, 0], pizza.parsePizzaVote(["null"]))
+        self.assertEqual([0, 0, 0, 0, 0], pizza.parsePizzaVote(["{}"]))
 
     def test_issue2_ifneedbe(self):
-        self.assertEqual([1, 0.5, 1, 0.5, 0.5], pizzaparser.parsePizzaVote(["mo,", "(di),", "mi,", "(do-fr)"]))
+        self.assertEqual([1, 0.5, 1, 0.5, 0.5], pizza.parsePizzaVote(["mo,", "(di),", "mi,", "(do-fr)"]))
 
     def test_issue3_deterministic_random(self):
-        firstDay = pizzaparser.choosePizzaDay(range(0, 5))
+        firstDay = pizza.choosePizzaDay(range(0, 5))
         for i in range(0,100):
-            self.assertEqual(firstDay, pizzaparser.choosePizzaDay(range(0, 5)))
+            self.assertEqual(firstDay, pizza.choosePizzaDay(range(0, 5)))
 
 if __name__ == '__main__':
     unittest.main()
