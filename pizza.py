7fec259c28db8fbd4781b4d537a84f23b4ec41c8
dragbone <ddragbone@gmail.com>
2016-08-26 20:30:27 +0200

    Refactored storing of votes

--- pizza.py
+++ pizza.py
@@ -4,8 +4,6 @@ import random
 import datetime
 from operator import add
 
-MEM_NAME = "pizzavotes"
-
 def _initialise(bot):
     plugins.register_user_command(["vote", "pizza"])
     plugins.register_admin_command(["resetvotes", "dumpvotes"])
@@ -17,10 +15,8 @@ indexToDay = {0:"Monday",
               4:"Friday"}
 
 def pizza(bot, event, *args):
-    initMemory(bot)
-
     votes = [0] * 5
-    mvotes = bot.memory.get_by_path([MEM_NAME])  # grab all votes
+    mvotes = getVotes(bot)
     for user_id in mvotes:
         vote = mvotes[user_id]
         votes = list(map(add, votes, vote))
@@ -33,39 +29,49 @@ def pizza(bot, event, *args):
     yield from bot.coro_send_message(event.conv, _("I recommend to eat pizza on {}").format(recommendedDay))
 
 def dumpvotes(bot, event, *args):
-    initMemory(bot)
-
-    mvotes = bot.memory.get_by_path([MEM_NAME])  # grab all votes
+    mvotes = getVotes(bot)
     for user_id in mvotes:
         vote = mvotes[user_id]
         yield from bot.coro_send_message(event.conv, _("Vote for " + user_id + ": {}").format(vote))
 
-def initMemory(bot):
-    if not bot.memory.exists([MEM_NAME]):
-        bot.memory.set_by_path([MEM_NAME], {})
-        bot.memory.save()
-
 def resetvotes(bot, event, *args):
-    initMemory(bot)
-
-    bot.memory.set_by_path([MEM_NAME], {})
-    bot.memory.save()
+    clearMemory(bot)
     yield from bot.coro_send_message(event.conv, _("Deleted all votes"))
 
 def vote(bot, event, *args):
-    initMemory(bot)
-
     if not args:
         yield from bot.coro_send_message(event.conv, _("Usage: /bot vote <i>Mon-Wed,(Fri)</i>\n/bot vote {}"))
         return
 
     try:
         vote = parsePizzaVote(args)
-        bot.memory.set_by_path([MEM_NAME, event.user.id_.chat_id], vote)
-        bot.memory.save()
+        storeVote(bot, event.user, vote)
         yield from bot.coro_send_message(event.conv, _("Thank you for voting ({})").format(vote))
     except Exception as ex:
         yield from bot.coro_send_message(event.conv, _("Error: Do you even syntax?! " + ex))
+
+# MEMORY
+# Stores votes and stuff (no pizzas, sorry!)
+MEM_NAME = "pizzavotes"
+
+def initMemory(bot):
+    if not bot.memory.exists([MEM_NAME]):
+        bot.memory.set_by_path([MEM_NAME], {})
+        bot.memory.save()
+
+def clearMemory(bot):
+    initMemory(bot)
+    bot.memory.set_by_path([MEM_NAME], {})
+    bot.memory.save()
+
+def storeVote(bot, user, vote):
+    initMemory(bot)
+    bot.memory.set_by_path([MEM_NAME, user.id_.chat_id], vote)
+    bot.memory.save()
+
+def getVotes(bot):
+    initMemory(bot)
+    return bot.memory.get_by_path([MEM_NAME])
 
 # PIZZAPARSER
 # Doesn't actually parse pizzas
